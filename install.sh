db553c09df963bf44555fccc962b3d4dee080d5a
Chuck Lantz <clantz@microsoft.com>
2021-01-23 22:00:44 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,31 +1,52 @@
-#!/bin/sh
+#!/bin/bash
 
-# https://github.com/idcrook/i-dotfiles
-# https://github.com/anishathalye/dotfiles
-# http://dotfiles.github.io
-# https://github.com/Kraymer/F-dotfiles 
+# Install curl, tar, git, other dependencies if missing
+PACKAGES_NEEDED="\
+    curl \
+    ca-certificates \
+    unzip \
+    git \
+    zsh"
 
-# Wrapper function to only use sudo if not already root
-sudoIf()
-{
-    if [ "$(id -u)" -ne 0 ]; then
-        sudo $@
-    else
-        $@
+if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        sudo apt-get update
     fi
-}
-
-# Install packages if on Debian / Ubuntu
-if type apt-get > /dev/null 2>&1; then
-    sudoIf apt-get install stow
-    sudoIf apt-get install bsdmainutils
-    
-    sudoIf apt-get install zsh git curl
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    chsh -s /usr/bin/zsh 
-
-    # https://github.com/powerline/fonts
-    sudoIf apt-get install fonts-powerline
+    sudo apt-get -y install ${PACKAGES_NEEDED}
 fi
 
-stow zsh --adopt
+# Fonts
+if ! dpkg -s "fontconfig" > /dev/null 2>&1; then
+    mkdir -p $HOME/.local/share/fonts /tmp/cascadia
+    curl -sSL https://github.com/microsoft/cascadia-code/releases/download/v2009.22/CascadiaCode-2009.22.zip -o /tmp/cascadia.zip
+    unzip /tmp/cascadia.zip -d /tmp/cascadia
+    mv /tmp/cascadia/ttf/*.ttf "$HOME/.local/share/fonts/" 
+    rm -rf /tmp/cascadia /tmp/cascadia.zip
+    curl -sSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o "$HOME/.local/share/fonts/MesloLGS NF Regular.ttf" 
+    curl -sSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -o "$HOME/.local/share/fonts/MesloLGS NF Bold.ttf"
+    curl -sSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -o "$HOME/.local/share/fonts/MesloLGS NF Italic.ttf"
+    curl -sSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -o "$HOME/.local/share/fonts/MesloLGS NF Bold Italic.ttf"
+    fc-cache -f -v
+fi
+
+# Oh My Zsh!
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    sudo chsh -s $(which zsh)
+fi
+
+# Add Tilix
+if ! grep 'TILIX_ID' ~/.bashrc > /dev/null 2>&1; then 
+tee -a "$HOME/.bashrc" > /dev/null \
+<< EOF
+
+# Add Tilix
+if [ \$TILIX_ID ] || [ \$VTE_VERSION ]; then
+  source /etc/profile.d/vte*.sh
+fi
+EOF
+fi
+
+cp -f .zshrc $HOME
+cp -f .p10k.zsh $HOME
